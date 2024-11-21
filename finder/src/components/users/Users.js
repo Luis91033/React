1a2e1f5ada4abb5e78e2bd585ec386b19e98b628
Luis91033 <luiscorralcorona@gmail.com>
2024-11-20 20:32:25 -0600

    [ADDED] Spinner

--- finder/src/components/users/Users.js
+++ finder/src/components/users/Users.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
 
-export class Users extends Component {
-   
-  render() {
-    return (
-      <div style ={userStyle}>
-        
-      </div>
-    )
-  }
+const Users = ({users, loading}) => {
+
+    if(loading){
+      return <Spinner/>
+    }else {
+      return (
+        <div style ={userStyle}>
+          {users.map(user => (
+            <UserItem  key={user.id} user={user}/>
+          ))}
+        </div>
+      )
+    }
+}
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 }
 
 const userStyle = {
     display:'grid',
-    gridTemplateColumns: ''
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
 }
 
 export default Users
