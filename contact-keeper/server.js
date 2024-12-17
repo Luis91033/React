049518fc5254ab8e0b54fe63723030b813db8dcb
Luis91033 <luiscorralcorona@gmail.com>
2024-12-16 19:49:13 -0600

    [ADDED] Connection with the database

--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');      
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello Word'));
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({extended:false}));
+
+app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API...'}));
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
