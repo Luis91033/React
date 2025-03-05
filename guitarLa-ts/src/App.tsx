40426a9e68cf7c68bfcec27c7e3be9f8fc163eb5
Luis91033 <luiscorralcorona@gmail.com>
2025-03-05 11:37:08 -0600

    [STARTED] GuitarLA with ts

---
+++ guitarLa-ts/src/App.tsx
@@ -0,0 +1,56 @@
+/** @format */
+
+import Headers from "./Components/Headers";
+import Guitar from "./components/Guitar";
+
+import useCart from "./hooks/useCart";
+
+function App() {
+  const {
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    decreaseQuantity,
+    increaseQuantity,
+    clearCart,
+    isEmpty,
+    cartTotal,
+  } = useCart();
+
+  return (
+    <>
+      <Headers
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+        isEmpty={isEmpty}
+        cartTotal={cartTotal}
+      />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+        <div className="row mt-5">
+          {data.map((guitarra) => (
+            <Guitar
+              key={guitarra.id}
+              guitarra={guitarra}
+              addToCart={addToCart}
+            />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
