0a6db7fb2136a55ee1ab617a3a8263a8e28b8280
Luis91033 <luiscorralcorona@gmail.com>
2025-03-31 23:21:43 -0600

    [FIXED] The performance of the views

--- bebidas-react-ts/src/router.tsx
+++ bebidas-react-ts/src/router.tsx
@@ -1,9 +1,11 @@
 /** @format */
-
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexPage from "./views/IndexPage";
-import FavoritesPage from "./views/FavoritesPage";
+
 import Layout from "./layouts/Layout";
+
+const Favoritespages = lazy(() => import("./views/FavoritesPage"));
 
 const Approuter = () => {
   return (
@@ -11,7 +13,14 @@ const Approuter = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<IndexPage />} index />
-          <Route path="/favoritos" element={<FavoritesPage />} />
+          <Route
+            path="/favoritos"
+            element={
+              <Suspense fallback="Cargando...">
+                <Favoritespages />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
