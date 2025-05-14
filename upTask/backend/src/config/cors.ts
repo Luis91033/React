8a611090e7ec9b899b6f8fafc3e1bc669a162062
Luis91033 <luiscorralcorona@gmail.com>
2025-05-14 11:01:40 -0600

    [FIXED] Cors error

--- upTask/backend/src/config/cors.ts
+++ upTask/backend/src/config/cors.ts
@@ -1,18 +1,19 @@
 /** @format */
 
-import { white } from "colors";
 import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
-  origin: function (origin, callback) {
+  origin: (origin, callback) => {
     const whitelist = [process.env.FRONTEND_URL];
+
     if (process.argv[2] === "--api") {
-      whitelist.push(undefined);
+      whitelist.push(undefined, null); // Permitir solicitudes sin origen (Postman, etc.)
     }
-    if (whitelist.includes(origin)) {
-      callback(null, true);
+
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true); // Permitir acceso
     } else {
-      callback(new Error("Error de CORS"));
+      callback(new Error("Error de CORS: origen no permitido")); // Bloquear acceso
     }
   },
 };
